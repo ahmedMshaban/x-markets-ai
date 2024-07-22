3c8dde4549e6b538a653502e3932cf80c633f70c
Salma Ali <salma_fouad@outlook.com>
2024-07-22 16:42:15 +0300

    Enhancing precision section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
+import EnhanceMarket from "@/components/home/EnhanceMarket";
 import { Header } from "./components";
+import CreateMarket from "@/components/home/CreateMarket";
+import ResolutionMethod from "@/components/home/ResolutionMethod";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <CreateMarket />
+        <EnhanceMarket />
+        <ResolutionMethod />
+      </main>
     </>
   );
 }
