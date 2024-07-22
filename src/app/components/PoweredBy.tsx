e24c10c89aa1597d4f5babe85d8d235218f7fb90
Ahmed Shaban <ahmed.shaban@spglobal.com>
2024-07-22 17:25:01 +0100

    Powerd by

---
+++ src/app/components/PoweredBy.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+
+export function PoweredBy() {
+  return (
+    <section className="w-[1070px] m-auto  py-10">
+      <h2 className="text-center color-[#413F47] text-[15px] font-bold leading-6 tracking-[1.5px] uppercase pb-2">
+        Powered By
+      </h2>
+      <div className="flex justify-center gap-[58px]">
+        <Image src="/assets/grok.svg" width={125} height={49} alt="Grok Logo" />
+        <Image
+          src="/assets/openai.svg"
+          width={129}
+          height={49}
+          alt="Open Ai Logo"
+        />
+        <Image
+          src="/assets/oracle.svg"
+          width={96}
+          height={49}
+          alt="Oracle Logo"
+        />
+        <Image src="/assets/uma.svg" width={45} height={45} alt="Uma Logo" />
+      </div>
+    </section>
+  );
+}
+
+export default PoweredBy;
--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,3 +1,4 @@
 export { Header } from "./Header";
 export { Hero } from "./Hero";
 export { Button } from "./ui/Button";
+export { PoweredBy } from "./PoweredBy";
