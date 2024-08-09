3ad9753d974dafba146cc658b3e3feeae588319b
Salma Ali <salma_fouad@outlook.com>
2024-08-09 15:52:56 +0300

    style tweaks

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,22 +3,20 @@ import Image from "next/image";
 export function Footer() {
   return (
     <footer className="pb-[37px] bg-[#151518]">
-      <div className="container max-w-1226 m-auto border-t border-[white] border-dashed pt-[37px] border-opacity-20">
-        <p className="flex items-center gap-[30px] text-[#ECECEC] text-[15px]">
-          <Image
-            src="/assets/footer-logo.svg"
-            width="39"
-            height="39"
-            alt="xMarkets Logo"
-          />
-          <span
-            className="opacity-60
+      <div className="flex flex-col items-center container text-center max-w-1226 m-auto border-t border-[white] border-dashed pt-[37px] border-opacity-20">
+        <Image
+          src="/assets/footer-logo.svg"
+          width={0}
+          height={0}
+          alt="xMarkets Logo"
+          className="h-auto w-[25.44px]"
+        />
+        <p
+          className="opacity-60
             font-semibold
-            tracking[-0.45px]
-          "
-          >
-            © 2024 xMarkets. All Rights Reserved.
-          </span>
+            tracking[-0.45px] mt-5 text-[#ECECEC] text-[15px]"
+        >
+          © 2024 xMarkets. All Rights Reserved.
         </p>
       </div>
     </footer>
--- src/app/components/PoweredBy.tsx
+++ src/app/components/PoweredBy.tsx
@@ -3,12 +3,24 @@ import Image from "next/image";
 export function PoweredBy() {
   return (
     <section className="container max-w-1070 m-auto  py-10">
-      <h2 className="text-center color-[#413F47] text-[15px] font-bold leading-6 tracking-[1.5px] uppercase pb-2">
+      <h2 className="text-center text-[#413F47] text-[15px] font-bold leading-6 tracking-[1.5px] uppercase pb-2">
         Powered By
       </h2>
       <div className="flex flex-wrap justify-center md:gap-[58px] gap-4">
-        <Image src="/assets/grok.svg" width={125} height={49} alt="Grok Logo" />
-        <Image src="/assets/uma.svg" width={45} height={45} alt="Uma Logo" />
+        <Image
+          src="/assets/grok.svg"
+          width={0}
+          height={0}
+          alt="Grok Logo"
+          className="h-auto w-[125px] "
+        />
+        <Image
+          src="/assets/uma.svg"
+          width={0}
+          height={0}
+          alt="Uma Logo"
+          className="h-auto w-[45px] "
+        />
       </div>
     </section>
   );
